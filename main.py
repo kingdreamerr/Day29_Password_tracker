fc1989649c0ee0141c7ab19f7ac144939d2dff31
kingdreamerr <jackmvula458@gmail.com>
2023-03-07 14:04:29 +0200

    Add pady=5 to the Entries and update README.md

--- main.py
+++ main.py
@@ -64,17 +64,17 @@ password_label.grid(row=3, column=0)
 
 #Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, columnspan=2,pady=5)
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2,pady=5)
+email_entry.insert(0, "kingdreamerr@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1,pady=5)
 
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2,pady=5)
 window.mainloop()
