1c21ab1bc982a9a462be4f075fa47394af6f0446
kingdreamerr <jackmvula458@gmail.com>
2023-03-07 13:45:40 +0200

    Add code to initialize app

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 # import pyperclip
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+window.mainloop()
